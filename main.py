3a37ab0d95ea8078c04114b9057e253254236c0a
chad <chad@chad.tw>
2016-12-20 20:22:51 +0800

    add search, settings, lists

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def login():
     global accountList,account
     username = raw_input("please enter the account : ")
     password = raw_input("please enter the password : ")
-    if accountList[username]['password'] == password:
-        account = username
-        print "\nWelcome, " + username
-        print "    Your " + accountList[username]['permission'] + '\n'
-    else:
-        print "Password not match"
+    try:
+        if accountList[username]['password'] == password:
+            account = username
+            print "\nWelcome, " + username
+            print "    Your " + accountList[username]['permission'] + '\n'
+        else:
+            print "Password not match"
+    except:
+        print "No such account. Fork u :p"
+        return 
 
 def submit():
     print "no such features yet"
@@ -30,28 +34,83 @@ def lists():
     classs = raw_input("Which class do you want to see? : ")
     i = 0
     print ""
-    for question in questionList[classs]:
-        print i+1, ". title = ", question['title']
-        i = i+1
-    print ""
+    try:
+        if len(questionList) == 0:
+            print "There is no question."
+        for question in questionList[classs]:
+            print i+1, ". title = ", question['title']
+            i = i+1
+        print ""
+    except:
+        print "No such class Bro"
+        return
     questionNumber = input("Which Question do you want? ")
+    if accountList[account]['permission'] == 'admin' and questionNumber == 0:
+        deleteNumber = input("Please enter the nubmer you want to delete(0 as cancel) ")
+        if deleteNumber == 0:
+            return 
+        else :
+            print "\n", questionList[classs][deleteNumber-1]['title'], "deleted!!", "\n"
+            questionList[classs].pop(deleteNumber-1)
+            return 
     print ""
-    question = questionList[classs][questionNumber-1]
-    print "class = ", classs
-    print "title = ", question['title']
-    print "name = " , question['questioner']
-    for answer in question['answer']:
-        print "#############################"
-        if answer['isBestAnswer'] == True:
-            print "This is Best Answer!!"
-        print "name = ", answer['name'], ", integral = ", accountList[answer['name']]['integral']
-        print "Answer = ", answer['data']
-    print ""
+    try:
+        question = questionList[classs][questionNumber-1]
+        print "class = ", classs
+        print "title = ", question['title']
+        print "name = " , question['questioner']
+        for answer in question['answer']:
+            print "#############################"
+            if answer['isBestAnswer'] == True:
+                print "This is Best Answer!!"
+            print "name = ", answer['name'], ", integral = ", accountList[answer['name']]['integral']
+            print "Answer = ", answer['data']
+        print ""
+    except:
+        print "No fucking Question bro"
+        return 
 
 def search():
-    pass
+    keyword = raw_input("\nInput your keyword : ")
+    for classs in questionList:
+        for question in questionList[classs]:
+            if keyword in question['title']:
+                print question['title']
+                try:
+                    print "class = ", classs
+                    print "title = ", question['title']
+                    print "name = " , question['questioner']
+                    for answer in question['answer']:
+                        print "#############################"
+                        if answer['isBestAnswer'] == True:
+                            print "This is Best Answer!!"
+                        print "name = ", answer['name'], ", integral = ", accountList[answer['name']]['integral']
+                        print "Answer = ", answer['data']
+                    print ""
+                except:
+                    print "No fucking Question bro"
+                    return 
+                    
+
 def settings():
-    pass
+    print "\n1.Add class"
+    print "2.Del class\n"
+    choise = raw_input("Enter your option : ")
+    if choise == '1':
+        classs = raw_input("Which class do you want to add(0 as cancel) : ")
+        if classs == '0':
+            return
+        else:
+             questionList[classs] = {}
+    elif choise == '2':
+        classs = raw_input("Which class do you want to delete(0 as cancel) : ")
+        if classs == '0':
+            return
+        else:
+            print classs, " deleted!!"
+            del questionList[classs]
+        pass
+    print ""
 
 while(1):
     if account == "":
@@ -77,8 +136,5 @@ while(1):
         elif choise == '3' and accountList[account]['permission'] == 'admin':
             settings()
         else:
-            print "fork u :p"
-                
-             
-
+            print "\nfork u :p\n"
 
